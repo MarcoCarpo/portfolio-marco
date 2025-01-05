8c1002c4ed2a1dfca1d6bfb8ae4f11f320cc28ee
Marco Carpona <marco.carpona@gmail.com>
2025-01-05 12:35:14 +0100

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Navbar from '@/components/Navbar';
 import { Toaster } from 'sonner';
 import Footer from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Toaster />
+        <Analytics />
         <Footer />
       </body>
     </html>
